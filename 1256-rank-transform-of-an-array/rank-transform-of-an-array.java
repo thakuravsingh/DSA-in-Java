bc1b9f9c295cec25f413d8eda06598284af8e5b5
thakuravsingh <thakuravsingh18@gmail.com>
2026-07-12 07:21:15 +0530

    Time: 27 ms (98.42%) | Memory: 77.2 MB (17.25%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,26 +1,15 @@
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int idx;
-        int val;
-        Pair(int idx,int val){
-            this.idx = idx;
-            this.val = val;
-        }
-        public int compareTo(Pair temp){
-            return Integer.compare(this.val,temp.val);
-        } 
-    }
     public int[] arrayRankTransform(int[] arr) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
         int n = arr.length;
-        for(int i = 0; i < n; i++) pq.add(new Pair(i,arr[i]));
-        int[] ans = new int[n];
+        int[] temp = new int[n];
+        for(int i = 0; i < n; i++) temp[i] = arr[i];
+        Arrays.sort(temp);
+        HashMap<Integer,Integer> map = new HashMap<>();
         int rank = 1;
-        while(!pq.isEmpty()){
-            Pair top = pq.remove();
-            ans[top.idx] = rank;
-            if(!pq.isEmpty() && top.val != pq.element().val) rank++;
+        for(int i = 0; i < n; i++){
+            if(!map.containsKey(temp[i])) map.put(temp[i],rank++);
         }
-        return ans;
+        for(int i = 0; i < n; i++) temp[i] = map.get(arr[i]);
+        return temp;
     }
 }
