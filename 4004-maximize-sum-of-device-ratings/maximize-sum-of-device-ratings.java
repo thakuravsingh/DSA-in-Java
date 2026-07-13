0aa8a8116e750472533c7353405a22c046d80337
thakuravsingh <thakuravsingh18@gmail.com>
2026-07-13 08:18:30 +0530

    Time: 39 ms (48.49%) | Memory: 210 MB (72.20%) - LeetSync

--- 4004-maximize-sum-of-device-ratings/maximize-sum-of-device-ratings.java
+++ 4004-maximize-sum-of-device-ratings/maximize-sum-of-device-ratings.java
@@ -8,20 +8,11 @@ class Solution {
             return rating;
         }
         for(int[] ele : units) Arrays.sort(ele);
-        int[] diff = new int[m];
-        for(int i = 0; i < m; i++) diff[i] = units[i][1] - units[i][0];
-        int idx = 0;
-        int min = Math.min(diff[0],units[0][0]);
         int firstMin = units[0][0];
         int secMin = units[0][1];
         for(int i = 1; i < m; i++){
             firstMin = Math.min(firstMin,units[i][0]);
             secMin = Math.min(secMin,units[i][1]);
-            int curMin = Math.min(diff[i],units[i][0]);
-            if(min > curMin){
-                idx = i;
-                min = curMin;
-            }
         }
         int count = 0;
         rating += firstMin;
