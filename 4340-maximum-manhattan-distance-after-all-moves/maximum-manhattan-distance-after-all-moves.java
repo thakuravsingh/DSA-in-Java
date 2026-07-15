1b5b2bf522b28a8cba4f71b3d2e0872ff75843f3
thakuravsingh <thakuravsingh18@gmail.com>
2026-07-15 08:42:01 +0530

    Time: 18 ms (36.07%) | Memory: 47.4 MB (98.30%) - LeetSync

--- 4340-maximum-manhattan-distance-after-all-moves/maximum-manhattan-distance-after-all-moves.java
+++ 4340-maximum-manhattan-distance-after-all-moves/maximum-manhattan-distance-after-all-moves.java
@@ -2,20 +2,16 @@ class Solution {
     public int maxDistance(String moves) {
         int n = moves.length();
         int ans = 0;
-        ArrayList<Character> list = new ArrayList<>();
         int dash = 0;
-        for(int i = 0; i < n; i++){
-            char ch = moves.charAt(i);
-            if(ch != '_') list.add(ch);
-            else dash++;
-        }
         int x = 0;
         int y = 0;
-        for(char ch : list){
+        for(int i = 0; i < n; i++){
+            char ch = moves.charAt(i);
             if(ch == 'U') y--;
             else if(ch == 'L') x--;
             else if(ch == 'R') x++;
-            else y++;
+            else if(ch == 'D') y++;
+            else dash++;
         }
         ans = Math.abs(x) + Math.abs(y);
         ans += dash;
