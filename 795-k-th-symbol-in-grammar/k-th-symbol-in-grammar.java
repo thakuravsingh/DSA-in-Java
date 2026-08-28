3ebe36b5c8a0a9641f33aec86d3edbb0d613ef06
thakuravsingh <thakuravsingh18@gmail.com>
2026-08-28 22:44:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (58.30%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(n == 1 && k == 1) return 0;
+        int mid = 1 << (n-2);
+        if(k <= mid) return kthGrammar(n-1,k);
+        return 1 - kthGrammar(n-1,k-mid);
+    }
+}
