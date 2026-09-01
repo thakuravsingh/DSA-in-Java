508ee957ffec79eafc6056f4fb9420268878af2f
thakuravsingh <thakuravsingh18@gmail.com>
2026-09-01 16:51:53 +0530

    Time: 3 ms (72.39%) | Memory: 47 MB (46.73%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,19 +1,17 @@
 class Solution {
-    int[] dp;
-    public int fxn(int[] freq, int idx) {
-        if (idx >= freq.length) return 0;
-        if (dp[idx] != -1) return dp[idx];
-        int pick = idx * freq[idx];
-        return dp[idx] = Math.max(fxn(freq, idx + 1), pick + fxn(freq, idx + 2));
-    }
     public int deleteAndEarn(int[] nums) {
-        int n = nums.length;
         int max = Integer.MIN_VALUE;
         for (int ele : nums) max = Math.max(max, ele);
         int[] freq = new int[max + 1];
         for (int ele : nums) freq[ele]++;
-        dp = new int[max + 1];
-        Arrays.fill(dp, -1);
-        return fxn(freq, 1);
+        int[] dp = new int[max + 1];
+        dp[1] = freq[1];
+        if(dp.length > 2) dp[2] = Math.max(freq[1],2*freq[2]);
+        for(int i = 3; i <= max; i++){
+            int pick = i * freq[i] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = Math.max(pick,skip);
+        }
+        return dp[max];
     }
 }
