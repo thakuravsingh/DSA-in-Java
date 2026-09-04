50e959da98bdefbd90819a42d28909a27be82172
thakuravsingh <thakuravsingh18@gmail.com>
2026-09-04 11:45:29 +0530

    Time: 728 ms (5.42%) | Memory: 47.1 MB (13.79%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
@@ -0,0 +1,33 @@
+class Solution {
+    int mod = (int)1e9+7;
+    int[][] dp;
+    public long pow(int a, int b){
+        if(b == 0) return 1;
+        if(b == 1) return a;
+        long ans = pow(a,b/2);
+        if(b % 2 == 0) return ans * ans;
+        else return a * ans * ans;
+
+    }
+    public int fxn(long[] nums, int idx, int target){
+        int n = nums.length;
+        if(target == 0) return 1;
+        if(target < 0) return 0;
+        if(idx >= n) return 0;
+        if(dp[idx][target] != -1) return dp[idx][target];
+        long sum = 0;
+        for(int i = idx; i < n; i++){
+            if(nums[i] > target) break;
+            long a = fxn(nums,i+1,(int)(target-nums[i]));
+            sum = (sum + a) % mod;
+        }
+        return dp[idx][target] = (int)sum;
+    }
+    public int numberOfWays(int n, int x) {
+        long[] nums = new long[n+1];
+        for(int i = 1; i <= n; i++) nums[i] = pow(i,x);
+        dp = new int[n+1][n+1];
+        for(int i = 0; i <= n; i++) Arrays.fill(dp[i],-1);;
+        return fxn(nums,1,n);
+    }
+}
