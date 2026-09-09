9d6b077ffd0827c23d373a59fe24ab7d31876b0c
thakuravsingh <thakuravsingh18@gmail.com>
2026-09-09 21:28:03 +0530

    Time: 2 ms (68.79%) | Memory: 45.1 MB (72.32%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,21 +1,23 @@
 class Solution {
-    int[][] dp;
-    public int minHealth(int[][] grid, int i, int j){
-        int m = grid.length;
-        int n = grid[0].length;
-        if(i >= m || j >= n) return (int)1e9;
-        if(i == m-1 && j == n - 1) return grid[i][j] <= 0 ? -grid[i][j] + 1 : 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        int r = minHealth(grid,i,j+1);
-        int d = minHealth(grid,i+1,j);
-        int res = grid[i][j] - Math.min(r,d);
-        return dp[i][j] = res < 0 ? -res : 1;
-    }
     public int calculateMinimumHP(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        dp = new int[m][n];
-        for(int i = 0; i < m; i++) Arrays.fill(dp[i],-1);
-        return minHealth(grid,0,0);
+        int[][] dp = new int[m][n];
+        dp[m-1][n-1] = grid[m-1][n-1] < 0 ? -grid[m-1][n-1] + 1 : 1;
+        for(int j = n - 2; j >= 0; j--){
+            int r = grid[m-1][j] - dp[m-1][j+1];
+            dp[m-1][j] = r < 0 ? -r : 1;
+        }
+        for(int i = m - 2; i >= 0; i--){
+            int d = grid[i][n-1] - dp[i+1][n-1];
+            dp[i][n-1] = d < 0 ? -d : 1;
+        }
+        for(int i = m - 2; i >= 0; i--){
+            for(int j = n - 2; j >= 0; j--){
+                int res = grid[i][j] - Math.min(dp[i][j+1],dp[i+1][j]);
+                dp[i][j] = res < 0 ? -res : 1;
+            }
+        }
+        return dp[0][0];
     }
 }
