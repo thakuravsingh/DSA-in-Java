93b63a82c426fcaa154be1b4a983fc6c6ab0e7da
thakuravsingh <thakuravsingh18@gmail.com>
2026-09-16 00:55:45 +0530

    Time: 10 ms (51.95%) | Memory: 46.9 MB (18.42%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,6 +1,9 @@
 class Solution {
+    int [][] dp;
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
+        dp = new int[n][2001];
+        for(int i = 0; i < n; i++) Arrays.fill(dp[i],-1);
         return findWays(nums,target,0,0);
     }
     public int findWays(int[] nums, int tgt, int i, int cur){
@@ -9,8 +12,9 @@ class Solution {
             if(cur == tgt) return 1;
             else return 0;
         }
+        if(dp[i][cur+1000]!=-1) return dp[i][cur+1000];
         int minus = findWays(nums,tgt,i+1,cur-nums[i]);
         int add = findWays(nums,tgt,i+1,cur+nums[i]);
-        return minus+add;
+        return dp[i][cur+1000] = minus+add;
     }
 }
