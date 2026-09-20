2714e9ee9afbfdd97785a223894058a9118baf2d
thakuravsingh <thakuravsingh18@gmail.com>
2026-09-20 17:16:45 +0530

    Time: 43 ms (60.84%) | Memory: 47.7 MB (39.41%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -1,23 +1,19 @@
 class Solution {
     public int findTheLongestSubstring(String s) {
-        HashMap<String,Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         int n = s.length();
-        int[] ch = new int[5];
-        map.put("00000",-1);
         int ans = 0;
+        int mask = 0;
+        map.put(0,-1);
         for(int i = 0; i < n; i++){
             char alphabet = s.charAt(i);
-            if(alphabet == 'a') ch[0] = (ch[0] + 1) % 2;
-            else if(alphabet == 'e') ch[1] = (ch[1] + 1) % 2;
-            else if(alphabet == 'i') ch[2] = (ch[2] + 1) % 2;
-            else if(alphabet == 'o') ch[3] = (ch[3] + 1) % 2;
-            else if(alphabet == 'u') ch[4] = (ch[4] + 1) % 2;
-            StringBuilder sb = new StringBuilder();
-            for(int j = 0; j < 5; j++){
-                sb.append(ch[j]);
-            }
-            if(map.containsKey(sb.toString())) ans = Math.max(ans,i-map.get(sb.toString()));
-            else map.put(sb.toString(),i); 
+            if(alphabet == 'a') mask = mask ^ 1;
+            else if(alphabet == 'e') mask = mask ^ 2;
+            else if(alphabet == 'i') mask = mask ^ 4;
+            else if(alphabet == 'o') mask = mask ^ 8;
+            else if(alphabet == 'u') mask = mask ^ 16;
+            if(map.containsKey(mask)) ans = Math.max(ans,i-map.get(mask));
+            else map.put(mask,i);
         }
         return ans;
     }
