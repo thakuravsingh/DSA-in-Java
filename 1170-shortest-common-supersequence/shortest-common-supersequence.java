e58707dbb07158a2235e4e568f67330d2ef72d05
thakuravsingh <thakuravsingh18@gmail.com>
2026-09-23 11:17:34 +0530

    Time: 21 ms (89.69%) | Memory: 54.1 MB (24.47%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String shortestCommonSupersequence(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = m; int j = n;
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                sb.append(s1.charAt(i-1));
+                i--;
+                j--;
+            } else {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    sb.append(s1.charAt(i-1));
+                    i--;
+                }
+                else {
+                    sb.append(s2.charAt(j-1));
+                    j--;
+                }
+            }
+        }
+        while (i > 0) {
+            sb.append(s1.charAt(i - 1));
+            i--;
+        }
+
+        while (j > 0) {
+            sb.append(s2.charAt(j - 1));
+            j--;
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+}
