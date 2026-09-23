e6e6c9b2fb65c7a69fc8ab48c5777adff2ba2e87
thakuravsingh <thakuravsingh18@gmail.com>
2026-09-24 02:22:39 +0530

    Time: 1 ms (42.15%) | Memory: 42 MB (74.74%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,14 +1,18 @@
 class Solution {
+    int[][] dp;
     public int integerBreak(int n) {
         int[] nums = new int[n-1];
         for(int i = 0; i < nums.length; i++) nums[i] = i + 1;
+        dp = new int[n][n+1];
+        for(int i = 0; i < n; i++) Arrays.fill(dp[i],-1);
         return maxProduct(nums,0,n);
     }
     public int maxProduct(int[] nums, int i, int tgt){
         if(tgt == 0) return 1;
         if(i == nums.length) return 1;
+        if(dp[i][tgt] != -1) return dp[i][tgt];
         int skip = maxProduct(nums,i+1,tgt);
         int pick = (tgt >= nums[i]) ? nums[i] * maxProduct(nums,i,tgt-nums[i]) : -(int)1e9;
-        return Math.max(skip,pick);
+        return dp[i][tgt] = Math.max(skip,pick);
     }
 }
